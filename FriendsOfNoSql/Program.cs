759b01eecbd5c4d590e78212b3a05e6d2b8295b3
Alexander Tank <Alexander.Tank@comparex.de>
2018-08-30 17:46:43 +0200

    init

--- FriendsOfNoSql/Program.cs
+++ FriendsOfNoSql/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using Marten;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FriendsOfNoSql
 {
-    class Program
+    public class Customer
     {
-        static void Main(string[] args)
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<Hobby> Hobbies { get; set; }
+    }
+
+    public class Hobby
+    {
+        public string Name { get; set; }
+    }
+
+    public class Program
+    {
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var store = DocumentStore.For(_ =>
+            {
+                _.AutoCreateSchemaObjects = AutoCreate.All;
+                _.Connection("host=localhost;database=marten_test;password=postgres;username=postgres");
+            });
+
+            using (var session = store.OpenSession())
+            {
+                var customer = new Customer
+                {
+                    Id = 1,
+                    Name = "Testbbbbb",
+                    Hobbies = new List<Hobby>
+                {
+                    new Hobby{Name = "Fischen"},
+                    new Hobby{Name = "Coden mit k"}
+                }
+                };
+
+                var hobby = new Hobby();
+                session.Store(customer);
+                await session.SaveChangesAsync();
+            }
+
+            using (var session = store.QuerySession())
+            {
+
+                var findById = session.Json.FindById<Customer>(1);
+                var hobbies = session.Query<Customer>()
+                    .Where(x => x.Hobbies.Any(h => h.Name == "Fischen"))
+                    .SelectMany(x => x.Hobbies)
+                    .Where(x => x.Name == "Fischen")
+                    .ToCommand().CommandText;
+            }
         }
     }
 }
