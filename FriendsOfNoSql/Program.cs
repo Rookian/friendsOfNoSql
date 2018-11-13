41fa95874e4ff1d1b85a55da795dda375e0be8ea
Alexander Tank <Alexander.Tank@comparex.com>
2018-11-13 17:09:28 +0100

    renaming migrations

--- FriendsOfNoSql/Program.cs
+++ FriendsOfNoSql/Program.cs
@@ -1,8 +1,14 @@
-﻿using Marten;
+﻿using System;
+using Marten;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Marten.Linq.SoftDeletes;
+using Npgsql;
+using SimpleMigrations;
+using SimpleMigrations.DatabaseProvider;
 
 namespace FriendsOfNoSql
 {
@@ -18,22 +24,49 @@ namespace FriendsOfNoSql
         public string Name { get; set; }
     }
 
-    public class Program
+    public class StoreFactory
     {
-        public static async Task Main()
+        public const string ConnectionString = "Server=friendsofnosqlpostgres.postgres.database.azure.com;Database=postgres;Port=5432;User Id=postgres@friendsofnosqlpostgres;Password=Password123!;SslMode=Require;";
+        public static DocumentStore CreateStore()
         {
+
             var store = DocumentStore.For(_ =>
             {
                 _.AutoCreateSchemaObjects = AutoCreate.All;
-                _.Connection("Server=friendsofnosqlpostgres.postgres.database.azure.com;Database=postgres;Port=5432;User Id=postgres@friendsofnosqlpostgres;Password=Password123!;SslMode=Require;");
+                _.Schema.For<Customer>();
+                _.Connection(ConnectionString);
                 //_.CreateDatabases = expressions => expressions.ForTenant().WithOwner("fo");
             });
+            return store;
+        }
+    }
 
+    public class Program
+    {
+
+        public static async Task Main()
+        {
+            var store = StoreFactory.CreateStore();
+
+            store.Schema.WritePatch("1.initial.sql");
+            var sql = store.Schema.ToDDL();
+            Debug.WriteLine(sql);
+            //store.Schema.AssertDatabaseMatchesConfiguration();
             store.Schema.ApplyAllConfiguredChangesToDatabase();
+
+            var migrationsAssembly = typeof(Program).Assembly;
+
+            using (var connection = new NpgsqlConnection(StoreFactory.ConnectionString))
+            {
+                var databaseProvider = new PostgresqlDatabaseProvider(connection);
+                var migrator = new SimpleMigrator(migrationsAssembly, databaseProvider);
+                migrator.Load();
+                migrator.MigrateToLatest();
+            }
 
             using (var session = store.OpenSession())
             {
-                
+
                 var customer = new Customer
                 {
                     Id = 1,
@@ -59,6 +92,57 @@ namespace FriendsOfNoSql
                     .Where(x => x.Name == "Fischen")
                     .ToCommand().CommandText;
             }
+        }
+    }
+
+    [Migration(1, "Rename Customer name in Fullname")]
+    public class CreateCustomer : Migration
+    {
+        private DocumentStore _documentStore;
+
+        public CreateCustomer()
+        {
+            _documentStore = StoreFactory.CreateStore();
+        }
+
+        protected override void Up()
+        {
+            using (var documentSession = _documentStore.OpenSession())
+            {
+                documentSession.Patch<Customer>(x => true).Rename("", x => x.Name);
+                documentSession.SaveChanges();
+            }
+        }
+
+        protected override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    [Migration(2, "#2 Rename Customer name in Fullname")]
+    public class Bla : Migration
+    {
+        private DocumentStore _documentStore;
+
+        public Bla()
+        {
+            _documentStore = StoreFactory.CreateStore();
+        }
+
+        protected override void Up()
+        {
+            using (var documentSession = _documentStore.OpenSession())
+            {
+                documentSession.Patch<Customer>(x => true).Rename("FullName", x => x.Name);
+                documentSession.SaveChanges();
+            }
+        }
+
+        protected override void Down()
+        {
+            throw new NotImplementedException();
         }
     }
 }
